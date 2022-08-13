450694327ca3fd41d546a6945a7cdd01790af02b
jimbrian <ui.ux.jimbrian@gmail.com>
2022-08-13 17:43:24 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Jim Brian';
+let welcomeMessage = 'Welcome to Flatbook,' + ' ' + currentUser + '!';
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK,' + ' ' + currentUser.toUpperCase() + '!';
+let shortGreeting = 'Welcome, ';
+let firstInitial = currentUser.slice(0, 3);
+console.log(firstInitial)
+let restOfName = currentUser.slice(3);
+console.log(restOfName)
+shortGreeting = `${shortGreeting} ${firstInitial}!`;
